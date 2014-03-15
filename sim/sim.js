9dec569675a59b365c5eafd70ae846fe3163a890
Per-Olov Jernberg <possan@possan.se>
2014-03-15 16:53:51 +0100

    Moar häx

--- sim/sim.js
+++ sim/sim.js
@@ -5,33 +5,53 @@
 		this.y = 0;
 		this.vx = 0;
 		this.vy = 0;
+		this.fx = 0;
+		this.fy = 0;
 		this.life = 0;
 		this.age = 0;
 		this.alive = false;
 	}
 
-	Particle.prototype.step = function(deltaTime) {
-		this.x += this.vx * deltaTime;
-		this.y += this.vy * deltaTime;
-		this.vx *= (1.0 - 0.25 * deltaTime);
-		this.vy *= (1.0 - 0.25 * deltaTime);
+	Particle.prototype.step = function(deltaTime, drag) {
+		var dragdelta = drag * deltaTime;
+		this.vx += this.fx * dragdelta;
+		this.vy += this.fy * dragdelta;
+		this.vx *= 1.0 - dragdelta;
+		this.vy *= 1.0 - dragdelta;
+		this.x += this.vx * dragdelta;
+		this.y += this.vy * dragdelta;
 		this.age += deltaTime;
-		if (this.age > 100.0) {
+		this.life -= deltaTime;
+		if (this.age > 100.0 || this.life < 0.0) {
 			this.alive = false;
 		}
 	}
 
-	Particle.prototype.addForce = function(wx, wy, deltaTime) {
-		this.vx += wx * deltaTime;
-		this.vy += wy * deltaTime;
+	Particle.prototype.resetForce = function() {
+		this.fx = 0;
+		this.fy = 0;
+	}
+
+	// Particle.prototype.addDrag = function(drag) {
+	// var drag = 0.6 - 0.3 * density;
+	// this.vx *= (1.0 - drag * deltaTime);
+	// this.vy *= (1.0 - drag * deltaTime);
+	// }
+
+	Particle.prototype.addForce = function(wx, wy) {
+		this.fx += wx;
+		this.fy += wy;
 	}
 
 	Particle.prototype.init = function(x, y, vx, vy) {
 		this.x = x;
 		this.y = y;
+		this.fx = 0;
+		this.fy = 0;
 		this.vx = vx;
 		this.vy = vy;
 		this.age = 0;
+		this.life = 60;
 		this.alive = true;
 	}
 
@@ -43,6 +63,13 @@
 		this.particles = [];
 		this.nextparticle = 0;
 		this.testpoint = { x: 0, y: 0 };
+		this.showVectors = false;
+		this.showDepth = false;
+		this.showFlow = false;
+		this.showDensity = false;
+		this.showOverlay = true;
+		this.running = false;
+		this.rainFlow = 0;
 	}
 
 	var loadPixelData = function(url) {
@@ -56,7 +83,6 @@
 			var can = document.createElement('canvas');
 			can.width = img.width;
 			can.height = img.height;
-			document.body.appendChild(can);
 			var ctx = can.getContext('2d');
 			ctx.width = img.width;
 			ctx.height = img.height;
@@ -64,15 +90,13 @@
 			ctx.drawImage(img, 0, 0, img.width, img.height);
 			console.log('context', ctx);
 			var imagedata = ctx.getImageData(0, 0, img.width, img.height);
-			ret.pixels = new Array(img.width * img.height);
+			ret.red = new Array(img.width * img.height);
+			ret.green = new Array(img.width * img.height);
 			for(var k=0; k<img.width*img.height; k++) {
-				ret.pixels[k] = imagedata.data[(k * 4)];
-				// if (k % 1000 == 0) {
-				//	console.log(k, ret.pixels[k]);
-				// }
+				ret.red[k] = imagedata.data[(k * 4)];
+				ret.green[k] = imagedata.data[(k * 4) + 1];
 			}
 			ret.image = img;
-			// console.log('ret', ret);
 		};
 		img.src = url;
 		return ret;
@@ -87,22 +111,32 @@
 		this.nextparticle = 0;
 		this.canvas = opts.canvas;
 		this.ctx = this.canvas.getContext('2d');
+		this.ctx.imageSmoothingEnabled = false;
+		this.ctx.webkitImageSmoothingEnabled = false;
 		this.width = opts.width;
 		this.height = opts.height;
 		this.canvas.width = this.width;
 		this.canvas.height = this.height;
 		this.wind = { x: 0, y: 0 };
+		this.dragging = false;
 		this.canvas.addEventListener('mousedown', function(e) {
-			_this.emit(e.x, e.y, 50, 10.0, 30.0);
+			_this.dragging = true;
+			_this.testpoint.x = e.offsetX * this.width / window.innerWidth;
+			_this.testpoint.y = e.offsetY * this.height / window.innerHeight;
 		});
 		this.canvas.addEventListener('mousemove', function(e) {
-			// _this.emit(e.x, e.y, 50, 10.0, 30.0);
-			_this.testpoint.x = e.x;
-			_this.testpoint.y = e.y;
+			_this.testpoint.x = e.offsetX * this.width / window.innerWidth;
+			_this.testpoint.y = e.offsetY * this.height / window.innerHeight;
+		});
+		this.canvas.addEventListener('mouseup', function(e) {
+			_this.dragging = false;
 		});
 		this.heightmap = loadPixelData(opts.heightmap);
+		this.flowmap = loadPixelData(opts.flowmap);
+		this.densitymap = loadPixelData(opts.densitymap);
+		this.overlaymap = loadPixelData(opts.overlaymap);
 		this.time = 0;
-
+		this.running = true;
 		opts.onLoad(this);
 	}
 
@@ -121,25 +155,25 @@
 		}
 	}
 
-	Simulation.prototype.getPixelValue = function(map, x, y) {
+	Simulation.prototype.getPixelValue = function(arr, x, y) {
 		if (x < 0 || y < 0 || x >= this.width || y >= this.height)
 			return 0;
 
-		if (map.pixels == null)
+		if (arr == null)
 			return 0;
 
 		var o = Math.round(y) * this.width + Math.round(x);
-		var val = map.pixels[o];
+		var val = arr[o];
 		return val;
 	}
 
 	Simulation.prototype.getSlope = function(x, y) {
-		var radii = 25;
-		var v0 = this.getPixelValue(this.heightmap, x - radii, y);
-		var v1 = this.getPixelValue(this.heightmap, x + radii, y);
-		var v2 = this.getPixelValue(this.heightmap, x, y - radii);
-		var v3 = this.getPixelValue(this.heightmap, x, y + radii);
-		// var v0 = this.getPixelValue(this.heightmap, x - 3, y);
+		var radii = 5;
+
+		var v0 = this.getPixelValue(this.heightmap.red, x - radii, y);
+		var v1 = this.getPixelValue(this.heightmap.red, x + radii, y);
+		var v2 = this.getPixelValue(this.heightmap.red, x, y - radii);
+		var v3 = this.getPixelValue(this.heightmap.red, x, y + radii);
 
 		var s = {
 			x: -(v1-v0),
@@ -156,19 +190,61 @@
 		return s;
 	}
 
+	Simulation.prototype.getFlow = function(x, y) {
+		var radii = 5;
+
+		var v0 = this.getPixelValue(this.flowmap.red, x, y);
+		var v1 = this.getPixelValue(this.flowmap.green, x, y);
+
+		var s = {
+			x: (v0 / 128.0) - 1.0,
+			y: (v1 / 128.0) - 1.0
+		};
+
+		var l = s.x*s.x + s.y*s.y;
+		if (l > 0) {
+			l = Math.sqrt(l);
+			s.x /= l;
+			s.y /= l;
+		}
+
+		return s;
+	}
+
+	Simulation.prototype.getDensity = function(x, y) {
+		var v0 = this.getPixelValue(this.densitymap.red, x, y);
+		return v0 / 255.0;
+	}
+
 	Simulation.prototype.step = function(deltaTime) {
 		var _this = this;
 		// console.log('sim::step', this.wind);
 		this.time += deltaTime;
 		this.particles.forEach(function(p) {
+			if (!p.alive)
+				return;
+			var d = _this.getDensity(p.x, p.y);
 			var s = _this.getSlope(p.x, p.y);
-			p.addForce(s.x * 10.0, s.y * 10.0, deltaTime);
-			p.addForce(_this.wind.x, _this.wind.y, deltaTime);
-			p.step(deltaTime);
+			// p.addForce(s.x * 10.0, s.y * 10.0, deltaTime);
+			var f = _this.getFlow(p.x, p.y);
+			// p.addForce(f.x * 10.0, f.y * 10.0, deltaTime);
+			p.resetForce();
+			p.addForce(p.vx, p.vy);
+			p.addForce(s.x, s.y);
+			p.addForce(f.x, f.y);
+			p.addForce(_this.wind.x, _this.wind.y);
+			// console.log('d', d);
+			// p.addDrag(0.1);
+			p.step(deltaTime, 1.0 - d);
 		});
-
-		var t = this.getPixelValue(this.heightmap, this.testpoint.x, this.testpoint.y);
-		// console.log('t='+t);
+		for(var i=0; i<this.rainFlow; i++) {
+			var rx = Math.random() * sim.width;
+			var ry = Math.random() * sim.height;
+			sim.emit(rx, ry, 1, 1.0, 1.0);
+		}
+		if (_this.dragging) {
+			_this.emit(this.testpoint.x, this.testpoint.y, 15, 30.0, 15.0);
+		}
 	}
 
 	Simulation.prototype.draw = function() {
@@ -177,9 +253,24 @@
 		this.ctx.fillStyle = '#111';
 		this.ctx.fillRect(0, 0, this.width, this.height);
 
-		if (this.heightmap.image) {
+		if (this.overlaymap.image && this.showOverlay) {
+			this.ctx.globalAlpha = 1.0;
+			this.ctx.drawImage(this.overlaymap.image, 0, 0, this.width, this.height);
+		}
+
+		if (this.flowmap.image && this.showFlow) {
 			this.ctx.globalAlpha = 0.5;
-			this.ctx.drawImage(this.heightmap.image, 0, 0);
+			this.ctx.drawImage(this.flowmap.image, 0, 0, this.width, this.height);
+		}
+
+		if (this.heightmap.image && this.showDepth) {
+			this.ctx.globalAlpha = 0.5;
+			this.ctx.drawImage(this.heightmap.image, 0, 0, this.width, this.height);
+		}
+
+		if (this.densitymap.image && this.showDensity) {
+			this.ctx.globalAlpha = 0.5;
+			this.ctx.drawImage(this.densitymap.image, 0, 0, this.width, this.height);
 		}
 
 		this.ctx.globalAlpha = 1.0;
@@ -190,57 +281,66 @@
 			if (!p.alive)
 				return;
 
-			_this.ctx.beginPath();
-			_this.ctx.moveTo(p.x, p.y);
-			_this.ctx.lineTo(p.x + p.vx * 2.0, p.y + p.vy * 2.0);
-     		_this.ctx.lineWidth = 1;
-     		_this.ctx.strokeStyle = 'green';
-     		_this.ctx.stroke();
+			if (_this.showVectors) {
+				_this.ctx.beginPath();
+				_this.ctx.moveTo(p.x, p.y);
+				_this.ctx.lineTo(p.x + p.vx * 1.0, p.y + p.vy * 1.0);
+	     		_this.ctx.lineWidth = 0.33;
+	     		_this.ctx.strokeStyle = 'cyan';
+	     		_this.ctx.stroke();
+     		}
 
 			_this.ctx.beginPath();
-			_this.ctx.moveTo(p.x-3, p.y);
-     		_this.ctx.lineTo(p.x+3, p.y);
-			_this.ctx.moveTo(p.x, p.y-3);
-     		_this.ctx.lineTo(p.x, p.y+3);
-     		_this.ctx.lineWidth = 1;
+			_this.ctx.arc(p.x, p.y, p.life/20.0, 0, 2*Math.PI);
+     		_this.ctx.lineWidth = 1.0;
      		_this.ctx.strokeStyle = 'white';
      		_this.ctx.stroke();
 		});
 
-		this.ctx.beginPath();
-		this.ctx.moveTo(this.testpoint.x - 50, this.testpoint.y);
- 		this.ctx.lineTo(this.testpoint.x + 50, this.testpoint.y);
-		this.ctx.moveTo(this.testpoint.x, this.testpoint.y - 50);
- 		this.ctx.lineTo(this.testpoint.x, this.testpoint.y + 50);
-		this.ctx.moveTo(this.testpoint.x, this.testpoint.y);
-		var s = this.getSlope(this.testpoint.x, this.testpoint.y);
-//		console.log(s);
- 		this.ctx.lineTo(this.testpoint.x + s.x * 50, this.testpoint.y + s.y * 50);
- 		this.ctx.lineWidth = 1;
- 		this.ctx.strokeStyle = 'yellow';
- 		this.ctx.stroke();
+		if (this.showVectors) {
+			this.ctx.beginPath();
+			this.ctx.arc(this.testpoint.x, this.testpoint.y, 50, 0, 2*Math.PI);
+	 		this.ctx.lineWidth = 1;
+	 		this.ctx.strokeStyle = 'white';
+	 		this.ctx.stroke();
 
+			this.ctx.beginPath();
+			var s = this.getSlope(this.testpoint.x, this.testpoint.y);
+			this.ctx.moveTo(this.testpoint.x, this.testpoint.y);
+	 		this.ctx.lineTo(this.testpoint.x + s.x * 50, this.testpoint.y + s.y * 50);
+	 		this.ctx.lineWidth = 1;
+	 		this.ctx.strokeStyle = 'yellow';
+	 		this.ctx.stroke();
 
+			this.ctx.beginPath();
+			var f = this.getFlow(this.testpoint.x, this.testpoint.y);
+			this.ctx.moveTo(this.testpoint.x, this.testpoint.y);
+	 		this.ctx.lineTo(this.testpoint.x + f.x * 50, this.testpoint.y + f.y * 50);
+	 		this.ctx.lineWidth = 1;
+	 		this.ctx.strokeStyle = 'red';
+	 		this.ctx.stroke();
+	 	}
 	}
 
 	var sim = new Simulation();
 
-	var ui_raining = true;
 	var ui_running = true;
 
 	var renderFrame = function() {
 		// console.log('render frame');
-		if (ui_raining) {
-			var rx = Math.random() * sim.width;
-			var ry = Math.random() * sim.height;
-			sim.emit(rx, ry, 1, 1.0, 10.0);
-		}
 		sim.draw();
-		if (ui_running) {
-			sim.step(1.0 / 60.0);
-		}
 		requestAnimationFrame(renderFrame);
 	};
+
+	var simulation_speed = 20;
+
+	var tick = function() {
+		if (ui_running) {
+			sim.step(1.0 / simulation_speed);
+		}
+	}
+
+	setInterval(tick, simulation_speed);
 
 	window.addEventListener('load', function() {
 		sim.init({
@@ -249,23 +349,51 @@
 			height: 768,
 			canvas: document.getElementById('c'),
 			heightmap: 'height.png',
-			friction: 'friction.png',
-			flow: 'flow.png',
+			flowmap: 'flow.png',
+			densitymap: 'density.png',
+			overlaymap: 'overlay.png',
 			onLoad: function() {
 				renderFrame();
 			}
 		});
-		document.getElementById('togglerain').addEventListener('click', function(e) {
-			ui_raining = !ui_raining;
-		})
 		document.getElementById('toggleplay').addEventListener('click', function(e) {
 			ui_running = !ui_running;
-		})
+		});
+		document.getElementById('togglevectors').addEventListener('click', function(e) {
+			sim.showVectors = !sim.showVectors;
+		});
+		document.getElementById('toggledepth').addEventListener('click', function(e) {
+			sim.showDepth = !sim.showDepth;
+		});
+		document.getElementById('toggledensity').addEventListener('click', function(e) {
+			sim.showDensity = !sim.showDensity;
+		});
+		document.getElementById('toggleflow').addEventListener('click', function(e) {
+			sim.showFlow = !sim.showFlow;
+		});
+		document.getElementById('toggleoverlay').addEventListener('click', function(e) {
+			sim.showOverlay = !sim.showOverlay;
+		});
 		document.getElementById('wind_x').addEventListener('change', function(e) {
 			sim.wind.x = document.getElementById('wind_x').value / 10.0;
 		});
 		document.getElementById('wind_y').addEventListener('change', function(e) {
 			sim.wind.y = document.getElementById('wind_y').value / 10.0;
+		});
+		document.getElementById('rain').addEventListener('change', function(e) {
+			sim.rainFlow = document.getElementById('rain').value;
+		});
+		document.addEventListener('keydown', function(e) {
+			if (e.keyCode == 32) {
+				document.getElementById('controls').style.display = 'block';
+			}
+			if (e.keyCode == 27) {
+				if (document.getElementById('controls').style.display == 'none') {
+					document.getElementById('controls').style.display = 'block';
+				} else {
+					document.getElementById('controls').style.display = 'none';
+				}
+			}
 		});
 	});
 
